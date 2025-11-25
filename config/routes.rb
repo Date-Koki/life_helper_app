53affdf2c1994bb5b2e6b06fb827f77aa064ffff
Date-Koki <k.date.0320@gmail.com>
2025-11-25 23:10:16 +0900

    AIによる自動生成機能

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   resources :lifehack_posts do
     resources :likes, only: [:create, :destroy]
   end
+  resources :living_todos, only: [:new, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
---
+++ spec/requests/living_todos_request_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "LivingTodos", type: :request do
+
+end
